c2f31ae5b9ab6ed287e493e0b3062182ce3f347d
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2024-08-12 16:15:02 +0530

    functions in php

---
+++ functions.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    function sum($a, $b)
+    {
+        echo $a + $b;
+    }
+    sum(2, 3);
+    echo "<br>";
+    function printFive()
+    {
+        echo "Five";
+    }
+    printFive();
+    ?>
+</body>
+
+</html>
